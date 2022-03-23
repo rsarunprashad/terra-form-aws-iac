6ed8af69c29e0915d669ac09b79a2331c9947ece
thamizhini arunprashad <thamizhini@thamizhinisMBP2.attlocal.net>
2022-03-23 04:56:39 -0500

    c1

--- main.tf
+++ main.tf
@@ -2,9 +2,22 @@ provider "aws" {
     region= "us-east-1"  
 }
 
+resource "aws_instance" "tf_instance1" {
+    ami = var.instance_small_ami
+    instance_type = var.instance_small
+    tags = {
+      "Name" = "tf_instance1"
+      "env" = "dev_test"
+    }
+  
+}
+/*
 
-
-
+resource "aws_ebs_volume" "ebs_east1b_type1" {
+    size = 25
+    availability_zone = "us-east-1b"
+  
+}
 
 resource "aws_volume_attachment" "ec2_ebs_type1" {
     device_name = "/dev/ebs1"
@@ -13,3 +26,5 @@ resource "aws_volume_attachment" "ec2_ebs_type1" {
 
   
 }
+
+*/
